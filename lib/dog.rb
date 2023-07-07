405d54607124fdfad5160f00e25bafa44bc58b75
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-07 21:12:16 +0300

    Done with lab test

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class Dog
-
+    def name=(dog_name)
+      # Setter method for the `name` attribute.
+      # It assigns the value of `dog_name` to the instance variable `@this_dogs_name`.
+      @this_dogs_name = dog_name
+    end
+  
+    def name
+      # Getter method for the `name` attribute.
+      # It returns the value of the instance variable `@this_dogs_name`.
+      @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"  
+
+perry = Dog.new
+perry.name = "Perry"
